15a475bec7681d512dcc4876a512969e00d95a9d
Sanket Kamble <1997sanket.k@gmail.com>
2022-03-24 01:19:19 +0530

    using Lambda to make the code even more concise

---
+++ src/Lambda.java
@@ -0,0 +1,15 @@
+//Using Lambda to make it even more concise.
+//Representation: Runnable runnable = ()->{ .... }; 
+
+public class Lambda {
+    public static void main(String[] args) {
+
+        Thread t = new Thread(() -> {
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(i);
+            }
+        });
+
+        t.start();
+    }
+}
